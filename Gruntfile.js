803aeb0074a31641d11621e1f3b2b14310b74d7d
Kananek Thongkam <kem@ns.co.th>
2016-10-25 23:14:50 +0700

    create gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,24 +1,97 @@
+var timer = require("grunt-timer");
+
 module.exports = function(grunt) {
+	timer.init(grunt, { deferLogs: true, friendlyTime: true, color: "blue" });
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    copy: {
-      js: { 
-        files: [{ expand: true, cwd: 'bower_components/jquery/dist/', src: 'jquery.js', dest: 'src/js/' }] 
+		clean: {
+		  javascript: ['sources/js/app.min.js'],
+		  scss: ['sources/css/site/**/*', 'sources/css/mintify/**/*'],
+		  dist: ['dist/**/*']
+		},
+    uglify: {
+      ie_fixed: {
+        files: { 
+        	'dist/ie-fixed.js': [
+	        	'sources/js/html5/*.js'
+        	] 
+        }
       },
-      css: { 
-        files: [{ expand: true, cwd: 'bower_components/jquery/dist/', src: 'jquery.js', dest: 'src/js/' }] 
+      app: {
+        files: { 
+        	'sources/js/app.min.js': [
+	        	'sources/app-js/**/*.js'
+        	] 
+        }
+      },
+      vendor: {
+        files: { 
+        	'dist/vendor.js': [
+	        	'sources/js/main/*.js',
+	        	'sources/js/*.js',
+	        	'sources/js/app.min.js'
+        	] 
+        }
       }
     },
-    uglify: {
-      app: {
-        files: { 'dist/app.js': ['src/js/*.js'] }
+	  sass: {
+	    dist: {
+	      files: [{
+	        expand: true,
+		      cwd: 'sources/app-scss',
+	        src: ['**/*.scss'],
+	        dest: 'sources/css/site',
+	        ext: '.css'
+	      }]
+	    }
+	  },
+    cssmin: {
+	   	vendor: {
+		    files: [{
+		      expand: true,
+		      cwd: 'sources/css',
+		      src: ['*.css'],
+		      dest: 'sources/css/mintify/vendor',
+		      ext: '.min.css'
+		    }]
+	  	},
+	   	site: {
+		    files: [{
+		      expand: true,
+		      cwd: 'sources/css/site',
+		      src: ['**/*.css'],
+		      dest: 'sources/css/mintify',
+		      ext: '.min.css'
+		    }]
+		  }
+    },
+	  concat_css: {
+	    options: {},
+	    all: {
+	      src: ["sources/css/mintify/**/*.css"],
+	      dest: "dist/app.min.css"
+	    },
+	  },
+    copy: {
+      fonts: { 
+        files: [{ expand: true, cwd:'sources/fonts/', src: '*.*', dest: 'dist/fonts/' }] 
+      },
+      icon: { 
+        files: [{ expand: true, cwd:'sources/icon/', src: '*.*', dest: 'dist/icon/' }] 
       }
-    }
+    },
   });
-   
+
+  grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-concat-css');
 
-  grunt.registerTask('default', ['copy']);
+  grunt.registerTask('js', ['uglify', 'sass']);
+  grunt.registerTask('css', ['cssmin','concat_css']);
+
+  grunt.registerTask('default', ['clean', 'js','css','copy']);
 };
