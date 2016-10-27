8375dbef82a0b5cffa8437f7c2b0666cb8a9608b
Kananek Thongkam <kem@ns.co.th>
2016-10-27 14:53:48 +0700

    change storage to IndexDB

--- sources/app-js/storage.js
+++ sources/app-js/storage.js
@@ -1,5 +1,17 @@
 window.Storage = function(KEY, setValue) {
 	KEY = KEY.toUpperCase();
+  // keyGlobal = keyGlobal || false;
+
+  // var CLIENT_STATE = (/CLIENT_STATE=(.*?)(;|$)/g.exec(document.cookie) || [])[1];
+  // var DATABASE_CODE = (/DATABASE_CODE=(.*?)(;|$)/g.exec(document.cookie) || [])[1];
+  // var store = CLIENT_STATE ? localforage.createInstance({ name: CLIENT_STATE }) : localforage;
+
+  // localforage.setItem(KEY, 'value', function (err) {
+  //   // if err is non-null, we got an error
+  //   localforage.getItem('key', function (err, value) {
+  //     // if err is non-null, we got an error. otherwise, value is the value 
+  //   });
+  // });
 	if(window.localStorage != undefined) {
   	var getValue = null;
 
