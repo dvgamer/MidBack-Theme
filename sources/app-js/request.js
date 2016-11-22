a08e7ed0e9941821efbbb721b10360d6b3850ebe
Kananek Thongkam <kem@ns.co.th>
2016-11-22 13:34:33 +0700

    add api optional in request and update table scss

--- sources/app-js/request.js
+++ sources/app-js/request.js
@@ -1,32 +1,45 @@
 window.request = function(options){ // 
 	_.defaults(options, {
 		url : location.href + /\/$/g.test(location.href) ? 'Default.aspx' : '',
+		api: false,
 		exception: false,
 		data: { },
 		callback: function(){ }
 	}); 
 
-  __.req.tasks.push(function(resolve, reject) {
+	if(!options.api) {
+	  __.req.tasks.push(function(resolve, reject) {
+			__.inst.post(options.url, {
+				data: options.data,
+			}).then(function (response) {
+				// console.wran('inst then', response);
+		  	resolve({ options: options, result: response.data }); 
+			}).catch(function (error) {
+				// console.wran('inst catch', error.message || error)
+			  reject({ options: options, result: error.message || error }); 
+			})
+	  });
+
+		__.unload = true;
+		window.preloader.on();
+		if(__.req.stopped) { 
+			__.req.stopped = false;
+			__.req.run().then(function(){ 
+				__.req.stopped = true; 
+				__.unload = false; 
+				window.preloader.off(); 
+			}); 
+		}
+		return __.req.tasks.length;
+	} else {
 		__.inst.post(options.url, {
 			data: options.data,
 		}).then(function (response) {
-			// console.wran('inst then', response);
-	  	resolve({ options: options, result: response.data }); 
+      var cb = new CallbackException(response.data);
+		  options.callback(cb.getItems, cb);
 		}).catch(function (error) {
-			// console.wran('inst catch', error.message || error)
-		  reject({ options: options, result: error.message || error }); 
+      var cb = new CallbackException(error.message || error);
+		  options.callback(cb.getItems, cb); 
 		})
-  });
-
-	__.unload = true;
-	window.preloader.on();
-	if(__.req.stopped) { 
-		__.req.stopped = false;
-		__.req.run().then(function(){ 
-			__.req.stopped = true; 
-			__.unload = false; 
-			window.preloader.off(); 
-		}); 
 	}
-	return __.req.tasks.length;
 }
